9a44286ab1241987cff537056c2c14c74bd5dda6
Henk van der Kuur <info@vanderkuur.com>
2020-10-26 16:12:29 +0100

    Methodes In structuur van het boek Fundamentals. Met getters en setters.

--- src/Auto.java
+++ src/Auto.java
@@ -1,6 +1,6 @@
 public class Auto {
 
-//Maak variabelen in de klasse Auto
+//Maak variabelen aan die in de klasse 'Auto' gebruikt gaan worden.
 
     int snelheid;
     String merk;
@@ -9,14 +9,16 @@ public class Auto {
     int verbruik;
 
 
-//Maak methodes aan die voor alle te maken objecten in de klasse Auto gebruikt kunnen worden.
+//Maak methodes aan die voor alle te maken objecten in de klasse 'Auto' gebruikt kunnen worden.
 
     void start() {
         System.out.println("De " + merk + " start.");
     }
+
     void stop() {
         System.out.println("De " + merk + " stopt.");
     }
+
     void geefGas() {
         snelheid += 20;
         kilometerstand += 20;
@@ -30,15 +32,37 @@ public class Auto {
     void setKilometerstand(int stand) {
         kilometerstand = stand;
     }
+
     int getKilometerstand() {
         return kilometerstand;
     }
 
-    void setMerk (String merk) {
+    void setMerk(String merk) {
         this.merk = merk;
     }
     String getMerk() {
         return merk;
+    }
+
+    void setSnelheid(int snelheid){
+        this.snelheid = snelheid;
+    }
+    int getSnelheid() {
+        return snelheid;
+    }
+
+    void setCapaciteit (int capaciteit){
+        this.capaciteit = capaciteit;
+    }
+    int getCapaciteit() {
+        return capaciteit;
+    }
+
+    void setVerbruik (int verbruik){
+        this.verbruik = verbruik;
+    }
+    int getVerbruik() {
+        return verbruik;
     }
 
 
@@ -50,16 +74,16 @@ public class Auto {
         Auto auto2 = new Auto();
         Auto auto3 = new Auto();
 
-        auto1.setMerk ("Toyota");
-        auto1.snelheid = 100;
-        auto1.capaciteit = 60;
-        auto1.verbruik = 10;
+        auto1.setMerk("Toyota");
+        auto1.setSnelheid(100);
+        auto1.setCapaciteit(60);
+        auto1.setVerbruik(10);
         auto1.setKilometerstand(20000);
 
-        auto2.setMerk ("Volvo");
-        auto2.snelheid = 120;
-        auto2.capaciteit = 80;
-        auto2.verbruik = 12;
+        auto2.setMerk("Volvo");
+        auto2.setSnelheid(120);
+        auto2.setCapaciteit(80);
+        auto2.setVerbruik(12);
         auto2.setKilometerstand(45000);
 
         auto3.setMerk("Mercedes");
@@ -71,27 +95,27 @@ public class Auto {
         auto1.start();
         auto1.stop();
         auto1.geefGas();
-        System.out.println("De snelheid van de "+auto1.merk+" is "+auto1.snelheid+" Km/uur.");
+        System.out.println("De snelheid van de " + auto1.getMerk() + " is " + auto1.getSnelheid() + " Km/uur.");
         auto1.geefGas();
-        System.out.println("De snelheid van de "+auto1.merk+" is "+auto1.snelheid+" Km/uur.");
+        System.out.println("De snelheid van de " + auto1.getMerk() + " is " + auto1.getSnelheid() + " Km/uur.");
         auto1.geefGas();
-        System.out.println("De snelheid van de "+auto1.merk+" is "+auto1.snelheid+" Km/uur.");
-        System.out.println("De kilometerstand van de "+auto1.merk+" is "+auto1.getKilometerstand()+" Km.");
-        System.out.println("De "+auto1.merk+" heeft een actieradius van " +auto1.bereik()+" Km.");
+        System.out.println("De snelheid van de " + auto1.getMerk() + " is " + auto1.getSnelheid() + " Km/uur.");
+        System.out.println("De kilometerstand van de " + auto1.getMerk() + " is " + auto1.getKilometerstand() + " Km.");
+        System.out.println("De " + auto1.getMerk() + " heeft een actieradius van " + auto1.bereik() + " Km.");
         System.out.println();
 
         auto2.start();
         auto2.stop();
         auto2.geefGas();
-        System.out.println("De snelheid van de "+auto2.merk+" is "+auto2.snelheid+" Km/uur.");
+        System.out.println("De snelheid van de " + auto2.getMerk() + " is " + auto2.getSnelheid() + " Km/uur.");
         auto2.geefGas();
-        System.out.println("De snelheid van de "+auto2.merk+" is "+auto2.snelheid+" Km/uur.");
+        System.out.println("De snelheid van de " + auto2.getMerk() + " is " + auto2.getSnelheid() + " Km/uur.");
         auto2.geefGas();
-        System.out.println("De snelheid van de "+auto2.merk+" is "+auto2.snelheid+" Km/uur.");
-        System.out.println("De kilometerstand van "+auto2.merk+ " is nu "+auto2.kilometerstand+ " Km.");
+        System.out.println("De snelheid van de " + auto2.getMerk() + " is " + auto2.getSnelheid() + " Km/uur.");
+        System.out.println("De kilometerstand van " + auto2.getMerk() + " is nu " + auto2.getKilometerstand() + " Km.");
         System.out.println();
 
-        System.out.println("Het merk van de derde auto is " +auto3.getMerk());
+        System.out.println("Het merk van de derde auto is " + auto3.getMerk()+".");
 
     }
 }
